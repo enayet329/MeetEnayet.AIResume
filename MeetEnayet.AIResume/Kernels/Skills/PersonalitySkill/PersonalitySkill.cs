ea3e7de65c65dcad1030b41d87e15a150a2907c6
Md Enayet Hossain <md.enayet.hossain329@gmail.com>
2025-07-29 13:02:41 +0600

    Add PersonalitySkill class with memory injection method
    Introduces the `PersonalitySkill` class in the `MeetEnayet.AIResume.Kernels.Skills.PersonalitySkill` namespace. This includes a private `_embedding` field of type `EmbeddingService`, a constructor for initialization, and a new asynchronous method `InjectPersonalityAsync` that retrieves relevant memory chunks and formats a response string with user input.

--- MeetEnayet.AIResume/Kernels/Skills/PersonalitySkill/PersonalitySkill.cs
+++ MeetEnayet.AIResume/Kernels/Skills/PersonalitySkill/PersonalitySkill.cs
@@ -1,6 +1,25 @@
-﻿namespace MeetEnayet.AIResume.Kernels.Skills.PersonalitySkill
+﻿using MeetEnayet.AIResume.Services;
+using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace MeetEnayet.AIResume.Kernels.Skills.PersonalitySkill
 {
 	public class PersonalitySkill
 	{
+		private readonly EmbeddingService _embedding;
+
+		public PersonalitySkill(EmbeddingService embedding)
+		{
+			_embedding = embedding;
+		}
+
+		[KernelFunction]
+
+		[Description("Injects personalized context from PDF")]
+		public async Task<string> InjectPersonalityAsync(string input)
+		{
+			var memory = await _embedding.GetRelevantChunksAsync(input);
+			return $"Memory:\n{memory}\nUser: {input}\nEnayetAI:";
+		}
 	}
 }
